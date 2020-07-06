05c606eea198ce2bc92897e0a50492584b9bd973
Dana204 <44040637+Dana204@users.noreply.github.com>
2020-07-06 14:30:05 -0500

    made inserts

--- SASAI_insert.sql
+++ SASAI_insert.sql
@@ -9,20 +9,40 @@ insert into Faculty_Info (faculty_id, faculty_name,title, dean_firstname, dean_l
 ('00006','Humanities and Education','Prof','Waibinte','Wariboko'),
 ('00007','Sport','Dr','Akshai','Mansingh');
 
-insert into Course(course_code, course_name, number_of_credits, level) values('COMP1126','Introduction to Computing I',3,1),('COMP1127','Introduction to Computing II',3,1);
+insert into Course(course_code, course_name, number_of_credits, level) values
+('COMP1126','Introduction to Computing I',3,1),
+('COMP1127','Introduction to Computing II',3,1),
+('COMP1161','Object-oriented Programming',3,1),
+('COMP1210','Math for Computing',3,1),
+('COMP2201','Discrete Mathematics for Computer Science',3,2),
+('COMP2211','Analysis of Algorithms',3,2),
+('COMP2140','Software Engineering',3,2),
+('COMP2170','Object Oriented Design and Implementation',3,2),
+('COMP2190','Net-Centric Computing',3,2),
+('COMP2340','Computer Organisation',3,2),
+('COMP3101','Operating Systems',3,3),
+('COMP3220','Introduction to Artificial Intelligence',3,3),
+('COMP3161','Database Management Systems',3,3),
+('COMP3901','Group Project',3,3);
 
 insert into Department_info(Department_id, Dept_name, dept_office_num, dept_email) values 
-('COMP','Computing','8767024455','computing@uwimona.edu.jm'),
-('GEOG','Geography and Geology','8769272129 ','geoggeol@uwimona.edu.jm'),
-('MATH','Mathematics','8769772693','maths@uwimona.edu.jm'),
-('PHYS','Physics','8769272480','physics@uwimona.edu.jm'),
-('CHEM','Chemistry','8769271910','hodchemistry@uwimona.edu.jm'),
-('BIOL','Life Sciences','8769271202','lifesci@uwimona.edu.jm');
+('COMP','Computing','876-702-4455','computing@uwimona.edu.jm'),
+('GEOG','Geography and Geology','876-927-2129 ','geoggeol@uwimona.edu.jm'),
+('MATH','Mathematics','876-977-2693','maths@uwimona.edu.jm'),
+('PHYS','Physics','876-927-2480','physics@uwimona.edu.jm'),
+('CHEM','Chemistry','876-927-1910','hodchemistry@uwimona.edu.jm'),
+('BIOL','Life Sciences','876-927-1202','lifesci@uwimona.edu.jm');
 
 insert into Degree_info(degree_id, degree_name, degree_type, elective_credits,incourse_credits, foundation_credits, level_1_credits, advanced_credits, total_credits) 
 values ('00012','Computer Science','BSc',21,45,15,30,30,90);
 
-insert into Lecturer(lecturer_id, lecturer_name) values('00021','Dr.Gunjun Mansingh');
+insert into Lecturer(lecturer_id, lecturer_name) values
+('00021','Dr. Gunjun Mansingh'),
+('00022','Dr. Donovan Campbell'),
+('00023','Dr. Nagarani Ponakala'),
+('00024','Dr. Tannecia Stephenson'),
+('00025','Dr. Roy Porter'),
+('00026','Dr. Dwight Robinson');
 
 insert into Faculty_email(faculty_id, email) values 
 ('00001','scitech@uwimona.edu.jm'),
@@ -33,7 +53,14 @@ insert into Faculty_email(faculty_id, email) values
 ('00006','fhe@uwimona.edu.jm');
 ('00007','sports@uwimona.edu.jm');
 
-insert into Faculty_phone_num(faculty_id, phone_number) values ('620088317','876-977-1785');
+insert into Faculty_phone_num(faculty_id, phone_number) values 
+('620088317','876-977-1785'),
+('00002','876-977-3536'),
+('00003','876-927-1855'),
+('00004','876-927-1297'),
+('00005','(876) 927-1640'),
+('00006','876-927-0221'),
+('00007','876-702-4473');
 
 insert into Student_email(student_id, email) values ('620088317','kingderwent@gmail.com');
 
